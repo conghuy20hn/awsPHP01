088a2705dff69de8dfe135848695ce4a81f7dc46
conghuy20hn <vt123456a@>
2018-09-06 10:57:07 +0700

    add check test aws 2

--- index.php
+++ index.php
@@ -2,16 +2,29 @@
 
 require 'vendor/autoload.php';
 
-function ServiceHandler() {
+function ServiceHandler()
+{
     $data = new stdClass();
     $data->Output = "Hello World! ServiceHandler";
 
     return json_encode($data);
-};
+}
+
+;
 
 $app = new \Slim\App;
 
 $app->get('/', 'ServiceHandler');
 $app->post('/', 'ServiceHandler');
+
+
+$app->get('/books', function ($request, $response, $args) {
+    // Show book identified by $args['id']
+    $data = new stdClass();
+    $data->Output = ["book1", "book2"];
+
+    return json_encode($data);
+});
+
 
 $app->run();
